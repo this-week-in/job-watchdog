37a216f951ed7d15250e6b7e233780639ee70726
Josh Long <starbuxman@gmail.com>
2018-01-18 02:39:39 -1000

    prefixing configuration properties.

--- src/main/kotlin/com/joshlong/jobs/watchdog/JobWatchdogAutoconfiguration.kt
+++ src/main/kotlin/com/joshlong/jobs/watchdog/JobWatchdogAutoconfiguration.kt
@@ -2,7 +2,7 @@ package com.joshlong.jobs.watchdog
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties("watchdog")
+@ConfigurationProperties(prefix = "watchdog")
 class WatchdogProperties(
 		/**
 		 * How long should the process run without any interaction before the termination function for the watchdog is invoked?
