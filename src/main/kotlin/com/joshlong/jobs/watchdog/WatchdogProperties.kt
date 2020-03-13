5486c88d292e30d223b6be260e95846cafa4d271
Josh Long <josh@joshlong.com>
2020-03-12 22:04:03 -0700

    config props for kotlin

--- src/main/kotlin/com/joshlong/jobs/watchdog/WatchdogProperties.kt
+++ src/main/kotlin/com/joshlong/jobs/watchdog/WatchdogProperties.kt
@@ -1,7 +1,9 @@
 package com.joshlong.jobs.watchdog
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 
+@ConstructorBinding
 @ConfigurationProperties(prefix = "watchdog")
 class WatchdogProperties(
 	var inactivityThresholdInSeconds: Long = 5 * 60,
